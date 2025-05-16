6491b9b3184a194792c2464e0c04b3157523be96
Rafael dos Santos <rafaeldossantosdasilva13@gmail.com>
2025-05-16 17:47:46 -0300

    Adicionando pesquisa de satisfação.

--- programa.py
+++ programa.py
@@ -1,2 +1,7 @@
 import requests
+
 print("Meu programa é relevante para a aula.") 
+
+nota = input("Numa escala de 0 a 1235, como você avalia a relevãncia desse projeto? ")
+
+print(f"Esse projeto tem uma relevãncia de {nota}") 
